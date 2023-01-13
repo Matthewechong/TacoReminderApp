080a1576aaad0264597c9cf47d34febbdf7bb593
matthewechong <matthewechong@gmail.com>
2023-01-12 19:37:07 -0800

    added PUT

--- app.py
+++ app.py
@@ -1,25 +1,68 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 from flask_restful import Resource, Api
 import sendEmail
-app = Flask("Taco Reminder API")
-api = Api(app)
+# app = Flask("Taco Reminder API")
+# api = Api(app)
 
 
-class TacoCount(Resource):
-    def get(self):
-        return sendEmail.points
+# class TacoCount(Resource):
+#     def get(self):
+#         return sendEmail.points
 
 
-class SendTacoCount(Resource):
-    def get(self):
-        try:
-            sendEmail.sendTacoCount()
-            return "Success"
-        except:
-            return "Failed"
+# class SendTacoCount(Resource):
+#     def get(self):
+#         try:
+#             sendEmail.sendTacoCount()
+#             return "Success"
+#         except:
+#             return "Failed"
+
+# api.add_resource(TacoCount, '/count')
+# api.add_resource(SendTacoCount, '/send')
+# if __name__ == '__main__':
+#     app.run()
+
+app = Flask(__name__)
 
 
-api.add_resource(TacoCount, '/count')
-api.add_resource(SendTacoCount, '/send')
-if __name__ == '__main__':
-    app.run()
+@app.route("/", methods=["POST", "GET"])
+def home():
+
+    # if request.method == "POST":
+    #     user = request.form["nm"]
+    #     count = request.form["count"]
+    #     print(user)
+    #     print(count)
+    #     data = sendEmail.findPerson(user)
+    #     data["taco_count"] = count
+    #     sendEmail.updatePerson(user,data)
+    #     sendEmail.sendTacoCount(user)
+    #     return redirect(url_for("user",usr=data))
+    # else:
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        count = request.form["count"]
+        print(user)
+        print(count)
+        data = sendEmail.findPerson(user)
+        data["taco_count"] = count
+        sendEmail.updatePerson(user, data)
+        sendEmail.sendTacoCount(user)
+        return redirect(url_for("user", usr=data))
+    else:
+        return render_template("index.html")
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>Name: {usr}</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- sendEmail.py
+++ sendEmail.py
@@ -2,27 +2,55 @@ import os
 from email.message import EmailMessage
 import ssl
 import smtplib
+import json
 email_sender = 'ScumBagChong@gmail.com'
-email_password = os.environ.get('email_password')
-email_receiver = 'wowomon123@gmail.com'
+email_password = 'qeaqhokdgiqugich'
 
 subject = 'Taco Count Reminder'
 points = 1
-body = f"""
-Top of the Morning
-The Name is Chong don't get it wrong
-One Point for Chong *clap
-Taco Points: {points}
-"""
+
 em = EmailMessage()
 em['From'] = email_sender
-em['To'] = email_receiver
 em['Subject'] = subject
-em.set_content(body)
+
 context = ssl.create_default_context()
 
 
-def sendTacoCount():
+def getMessage(points):
+    return f"""
+    Top of the Morning
+    The Name is Chong don't get it wrong
+    One Point for Chong *clap
+    Taco Points: {points}
+    """
+
+
+def readJson():
+    with open("data.json") as data:
+        json_str = data.read()
+        json_data = json.loads(json_str)
+        return json_data
+
+
+def findPerson(name):
+    json_data = readJson()
+    person_data = next(item for item in json_data if item["name"] == name)
+    return person_data
+
+
+def updatePerson(name, data):
+    json_data = readJson()
+    new_entry = [d for d in json_data if not (d["name"] == name)]
+    new_entry.append(data)
+    with open("data.json", "w") as file:
+        json.dump(new_entry, file)
+
+
+def sendTacoCount(name):
+    data = findPerson(name)
+    em['To'] = data["email"]
+    em.set_content(getMessage(data["taco_count"]))
+
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
+        smtp.sendmail(email_sender, data["email"], em.as_string())
